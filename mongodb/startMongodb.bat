28b9af8e3b1067d27883f67004d9a6207a69ec5f
root <173302591@qq.com>
2019-12-26 17:57:02 +0800

    +

---
+++ mongodb/startMongodb.bat
@@ -0,0 +1,4 @@
+cd C:\Program Files\MongoDB\Server\4.2\one\bin
+set %cd%=C:\Program Files\MongoDB\Server\4.2\one\bin
+start mongod.exe --config "C:\data\mongod1.cfg"
+start mongod.exe --config "C:\data\mongod2.cfg"
